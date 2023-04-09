df1fdc4fb51143580db012c47719e7fae2b1acee
therock111 <coc10214@gmail.com>
2023-04-10 00:33:46 +0530

    Uploading data

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/BasicCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicCrudOperationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/in28minutes/springboot/myfirstwebapp/BasicCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudOperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
