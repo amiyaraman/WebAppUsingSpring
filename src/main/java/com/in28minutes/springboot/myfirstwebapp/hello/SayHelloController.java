e68790854699d2b3dcc43efb7fe7c00e3e5646cf
therock111 <coc10214@gmail.com>
2023-04-10 00:33:55 +0530

    Uploading data

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,33 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("/say-hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello!what are you learning today";
+    }
+    @RequestMapping("/say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My First HTM page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<h1>Mea First HTML page</h1>");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
+    @RequestMapping("/say-hello-jsp")
+    public String sayHelloJsp(){
+        return "sayHello";
+    }
+}
